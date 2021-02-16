e7666305aff8cfc3ce9fda26946b97e5f6c09220
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 12:24:03 -0600

    Update Clase6.h

--- CursoC/Clase6.h
+++ CursoC/Clase6.h
@@ -1,4 +1,15 @@
 #ifndef CLASE6_H
 #define CLASE6_H
 
+#include "util.h"
+#include "Clase4.h"
+
+void c6_ejemplo();
+void c6_ejemplo1();
+void c6_ejemplo2();
+
+void guarda_persona(const persona *p);
+
+
+
 #endif // CLASE6_H
