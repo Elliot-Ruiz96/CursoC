467f25599a05048ae092880bd4a26946ef68d7ed
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-16 12:11:37 -0600

    Create Clase6.c

---
+++ CursoC/Clase6.c
@@ -0,0 +1,45 @@
+#include "Clase6.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 100
+
+void c6_ejemplo1()
+{
+    FILE *archivito = fopen ("archivito.txt", "w");
+    int valor = 15;
+
+    if(fprintf(archivito, "Escribiendo %i dentro del archivo \n", valor))
+    {
+        printf("El archivo fue escrito con exito 7n");
+    }
+    else
+    {
+        printf("Problemas al escribir el archivo");
+        ferror(archivito);
+    }
+    fclose(archivito);
+     //Es importante cerrar el archivo despues de usarlo, por si usas el archivo despues
+}
+
+void c6_ejemplo()
+{
+    FILE *archivito = fopen ("archivito.txt", "r");
+    fseek(archivito, 0, SEEK_END);
+    int archivito_p = ftell(archivito);
+    int leido;
+    do
+    {
+        leido = fgetc(archivito);
+        if(leido==EOF)
+            break;
+        printf("%c", leido);
+    }
+    while(leido != EOF);
+    char *buffer = (char*) malloc(BUFFER_SIZE*sizeof(char));
+    fseek(archivito, archivito_p, SEEK_END);
+    printf("Valor de tell: %i \n", archivito_p);
+    fgets(buffer, 10, archivito);
+    printf("%s \n", buffer);
+    fclose(archivito);
+}
