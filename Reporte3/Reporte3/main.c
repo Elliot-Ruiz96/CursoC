871c4055d2b68e3d48dcf6e93784283d9ea831f3
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-18 09:18:59 -0600

    Update main.c

--- Reporte3/Reporte3/main.c
+++ Reporte3/Reporte3/main.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-const int Tam_temp = 100;
+// Libreria para darle conidcion al ciclo while
+
+const int MAX_ARRAY = 100;
+// Evitar numeros magicos
 void invertir_palabra(char *palabra);
-void numero(int*);
+// Funcion dedicada a invertir la palabra
+
 
 int main(){
-    char palabra[Tam_temp];
+    char cadena[MAX_ARRAY];
+    // Tamaño maximo de la cadena
     printf("Ingresa una cadena sin espacio: \n");
-    scanf("%s",palabra);
-    invertir_palabra(palabra);
+    scanf("%s",cadena);
+    invertir_palabra(&cadena);
+    // Llamada a funcion y envio de punteros
 }
 
-void invertir_palabra(char *palabra){
+void invertir_palabra(char *cadena)
+// Palabra se declara como una "Variable puntero (tal como se menciona en la documentacion)"
+// Es iimportante mencionar que los char son considerados como punteros
+
+{
     char *ptrletrauno , *ptrletrados;
+    // Declaracion de dos nuevos punteros
     int resta = 1,Tam_array = 0;
+    // Variable resta a usar para el tamaño del arreglo en el ciclo for  debido al caracter nulo
     bool ciclo = true;
-    Tam_array = strlen(palabra);
+    Tam_array = strlen(cadena);
+    // Tamaño real de la cadena y sus caracteres ingresados para evitar basura
     while(ciclo){
         for(int i=0;i<Tam_array-resta;i++)
         {
-            ptrletrauno = palabra[i];
-            ptrletrados = palabra[i+1];
-            palabra[i] = ptrletrados;
-            palabra[i+1] = ptrletrauno;
+           ptrletrauno = &cadena[i];
+           ptrletrados = &cadena[i+1];
+            cadena[i] = ptrletrados;
+            cadena[i+1] = ptrletrauno;
+
+            // Tal como se explica arriba estos son apuntadores, no se estan almacenando en variables fijas
         }
         resta +=1;
+        // Da un cierre al ciclo while dando "i=tam_array - resta"
         if (Tam_array == resta){
             ciclo = false;
         }
     }
-    printf("%s\n",palabra);
+    printf("\nTu cadena invertida es: \n");
+    printf("%s\n\n",cadena);
 }
