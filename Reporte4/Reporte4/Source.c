25cb29d0af4703dc856cc9bdb63aaf9ceef6796f
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-02-22 00:04:39 -0600

    Update Source.c

--- Reporte4/Reporte4/Source.c
+++ Reporte4/Reporte4/Source.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+#include "Animal.h"
 
-int returnseven (void)
+#define max_animales 9 //Debido a que el arreglo empieza desde 0
+
+int num_animal=0, num_el_animal;
+
+
+void funcion_ingresar()
 {
-    return 7;
+    if (num_animal <= max_animales)
+    {
+        printf("\n1. Agregar animal:\n");
+        animal c[max_animales];
+        printf("\nIngrese la edad del animal #%d: ", num_animal+1);
+        scanf("%d", &c[num_animal].edad);
+        printf("Ingrese el nombre del animal #%d: ", num_animal+1);
+        scanf("%s", c[num_animal].nombre);
+        printf("Ingrese la especie del animal #%d: ", num_animal+1);
+        scanf("%s", c[num_animal].especie);
+        num_animal = num_animal+1;
+    }
+    else
+    {
+        printf ("Numero maximo de animales ingresados. \n");
+    }
+
+}
+
+void funcion_mostrar()
+{
+    printf("\n2. Mostrar animales:\n");
+    animal c[max_animales];
+    printf("Ingrese numero de animal a mostrar: ");
+    scanf("%d", &num_el_animal);
+    num_el_animal = num_el_animal-1;
+    printf("\nEdad del animal: %d\n", c[num_el_animal].edad);
+    printf("Nombre del animal: %s\n", c[num_el_animal].nombre);
+    printf("Especie del animal: %s\n", c[num_el_animal].especie);
 }
